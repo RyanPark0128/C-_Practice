1d2b0a964825e83d3a738d2d8a38679c6820ea85
RyanPark0128 <vazywon@hotmail.com>
2022-12-24 09:38:44 -0800

    cpp practice

--- test.cpp
+++ test.cpp
@@ -1,10 +1,28 @@
+//Lecture 1 2023-01-04
+
 #include <iostream>
 
 int main() {
-  int x;
-  std::cout << "enter a number" << std::endl;
-  std::cin >> x;
-  std::cout << "You entered " << x << std::endl;
+  int x = 42;
+  // if you add int to a string, corresponding ASCII char will be added
+  std::string s;
+  std::string y = "Hello World";
+  s += " world";
+  s += '!';
+  s += std::to_string(x);
+
+  // std::cout << "enter something" << std::endl;
+  // std::cin >> s;  // uses space as delimiter
+  // std::cout << "You entered " << s << std::endl;
+
+  // std::cout << "enter something" << std::endl;
+  // getline(std::cin,y); //reads the whole line as string
+  // std::cout << "You entered " << y << std::endl;
+
+  std::cout << y << std::endl;
+  y[0] = 'D'; // use single quote to specifically set it as char
+  std::cout << y << std::endl;
+
 
   return 0;
 }
