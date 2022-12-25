da7204ba7e8e708dcb49fe31ca709a4bddcee96d
RyanPark0128 <vazywon@hotmail.com>
2022-12-25 09:52:42 -0800

    cpp practice

--- test.cpp
+++ test.cpp
@@ -5,11 +5,17 @@
 int main() {
   int x = 42;
   // if you add int to a string, corresponding ASCII char will be added
+
+  // unsigned x = 42;
+  // only positive
+
   std::string s;
   std::string y = "Hello World";
   s += " world";
   s += '!';
   s += std::to_string(x);
+
+  std::cout << s.size() << std::endl; //length of string
 
   // std::cout << "enter something" << std::endl;
   // std::cin >> s;  // uses space as delimiter
@@ -19,9 +25,11 @@ int main() {
   // getline(std::cin,y); //reads the whole line as string
   // std::cout << "You entered " << y << std::endl;
 
-  std::cout << y << std::endl;
-  y[0] = 'D'; // use single quote to specifically set it as char
-  std::cout << y << std::endl;
+  std::cout << s << std::endl;
+  s[0] = 'D'; // use single quote to specifically set it as char
+  std::cout << s <<std::endl;
+
+  //for loops, if, while loops, are essentially the same
 
 
   return 0;
