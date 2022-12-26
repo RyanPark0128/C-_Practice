623afae10fc92880f37f1369f15e6308eb04ff16
RyanPark0128 <vazywon@hotmail.com>
2022-12-26 10:17:02 -0800

    cpp practice

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
 //Lecture 1 2023-01-04
 
 #include <iostream>
+#include <ctime> // time into computer, generate random values
 
 int main() {
+  srand(time(0));
   int x = 42;
   // if you add int to a string, corresponding ASCII char will be added
 
@@ -30,6 +32,13 @@ int main() {
   std::cout << s <<std::endl;
 
   //for loops, if, while loops, are essentially the same
+
+
+// s[i] vs s.at(i)   .at checks bound and breaks if we go out of bound
+for (int i=0; i < 10; i++) {
+  std::cout << (rand()%10 +1) << std::endl;
+}
+// rand() % 10 + 1     generates 1 to 10
 
 
   return 0;
