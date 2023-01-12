0d289e09708bf6ed2424b0376a8dc05994fbb15d
RyanPark0128 <vazywon@hotmail.com>
2023-01-12 09:55:13 -0800

    011623 lecture 2

--- 011623.cpp
+++ 011623.cpp
@@ -26,8 +26,31 @@ int sum_vect(const std::vector<int> & v, int l, int h) {
   return sum_vect(v,l,mid) + sum_vect(v,mid+1,h);
 }
 
+bool is_pali(const std::string & s, int i, int j) {
+  if (i >= j) {
+    return true;
+  }
+  if (s[i] != s[j]) {
+    return false;
+  }
+
+  return is_pali(s,i+1, j-1);
+}
+
+bool is_pali(std::string s) {
+  int length = s.size()-1;
+  return is_pali(s, 0, length);
+}
+
+
+
 int main () {
   std::vector<int> v = {1,2,3,4,5,6};
-  std::cout<< sum_vect(v,0,v.size()-1) <<std::endl;
+  // std::cout<< sum_vect(v,0,v.size()-1) <<std::endl;
+  std::cout << std::boolalpha << is_pali("racecar") <<std::endl;
+  std::cout << std::boolalpha << is_pali("abc") << std::endl;
+  std::cout << std::boolalpha << is_pali("tuyyut") <<std::endl;
+  std::cout << std::boolalpha << is_pali("tuy") <<std::endl; 
+  std::cout << std::boolalpha << is_pali("abba") <<std::endl;
   return 0;
 }
