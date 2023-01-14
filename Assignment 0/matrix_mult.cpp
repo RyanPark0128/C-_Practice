6b8921ad350b7b05e20b02d3df71fbeec28a9e2c
RyanPark0128 <vazywon@hotmail.com>
2023-01-14 12:37:34 -0800

    assignment 0 part 2

--- Assignment 0/matrix_mult.cpp
+++ Assignment 0/matrix_mult.cpp
@@ -3,52 +3,62 @@
 #include <vector>
 #include <iomanip>
 
- std::vector<std::vector<int> > readIn(std::string file, int & row, int & col){
+std::vector<std::vector<int> > readIn(std::string file){
     std::ifstream fin;
     fin.open(file);
+    int row,col;
     fin >> row;
     fin >> col;
     std::vector<std::vector<int> > m(row,std::vector<int>(col));
     for(int i = 0; i < row; i++){
         for(int j = 0 ; j < col ; j++){
             fin >> m[i][j];
-            std::cout << m[i][j] << " ";
         }
-
-        std::cout << std::endl;
     }
     return m;
-
 }
+
+void printMatrix(std::vector<std::vector<int>> & matrix) {
+    for (int i=0; i< matrix.size();i++) {
+        for (int j=0; j <matrix[i].size(); j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void multiplyMatrix(std::vector<std::vector<int>> & m1, std::vector<std::vector<int>> & m2, std::vector<std::vector<int>> & multi, int r1, int c2) {
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            multi[i][j] = 0;
+            for (int k = 0; k < m2.size(); k++) {
+                    multi[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+}
+
 int main(){
     std::cout << "enter the file name" << std::endl;
     std::string inputOne;
     std::cin >> inputOne;
-    int r1;
-    int c1;
-    std::vector<std::vector<int>> m1 = readIn(inputOne, r1, c1);
+    std::vector<std::vector<int>> m1 = readIn(inputOne);
+    int r1 = m1.size();
+    int c1 = m1[0].size();
 
     std::string inputTwo;
     std::cin >> inputTwo;
-    int r2;
-    int c2;
-    std::vector<std::vector<int>> m2 = readIn(inputTwo, r2, c2);
+    std::vector<std::vector<int>> m2 = readIn(inputTwo);
+    int r2= m2.size();
+    int c2 = m2[0].size();
 
     std::vector<std::vector<int>> multi (r1, std::vector<int>(c2));
 
     if (c1 == r2) {
-        for (int i = 0; i < r1; i++) {
-        for (int j = 0; j < c2; j++) {
-            multi[i][j] = 0;
-            for (int k = 0; k < r2; k++) {
-                multi[i][j] += m1[i][k] * m2[k][j];
-            }
-            std::cout << multi[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+        multiplyMatrix(m1,m2,multi,r1,c2);
+        printMatrix(multi);
     } else {
-        std::cout << " can not be multiplied" << std::endl;
+        std::cout << "Matrixes can not be multiplied" << std::endl;
     }
 
     return 0;
