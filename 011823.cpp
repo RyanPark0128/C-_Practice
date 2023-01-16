7681c3e3d4ea44fd76f55fcbf747bd3f9bb86881
RyanPark0128 <vazywon@hotmail.com>
2023-01-16 09:00:08 -0800

    011823 lecture 1

---
+++ 011823.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int bin_print (n) {
+  if (k==0) {
+    std::cout<< str <<std:: endl;
+    return;
+  }
+
+  bin_print(k-1,str)
+  bin_print(k-1,str)
+}
+int main() {
+  int k;
+  cin >> k;
+  bin_print(k)
+  return 0;
+}
