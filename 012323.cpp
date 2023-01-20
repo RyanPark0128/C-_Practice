a240b500f5be92fd745c3e5566d99a9ab289f405
RyanPark0128 <vazywon@hotmail.com>
2023-01-20 09:52:10 -0800

    012323 lecture 2

--- 012323.cpp
+++ 012323.cpp
@@ -13,8 +13,28 @@ int find_max(std::vector<int> v, int i =0) {
   }
   return v[i];
 }
+
+void selection_sort(std::vector<int> & v) {
+  for (int i=0; i< v.size(); i++) {
+    int min = i;
+    for (int j=i+1; j< v.size(); j++) {
+      if (v[min] > v[j]) {
+        min = j;
+      }
+    }
+    int temp = v[i];
+    v[i] = v[min];
+    v[min] = temp;
+  }
+}
+
 int main() {
   std::vector<int> v = {6,3,7,12,8,4,1};
-  std::cout << find_max(v) << std::endl;
+  // std::cout << selection_sort(v) << std::endl;
+  selection_sort(v);
+
+  for(auto x : v) {
+    std::cout << x << std::endl;
+  }
   return 0;
 }
