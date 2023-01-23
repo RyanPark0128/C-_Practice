a1eba4af7d1374c5ffb8bd5065b0e1d1824c55b8
RyanPark0128 <vazywon@hotmail.com>
2023-01-22 16:09:37 -0800

    assignment 1 complete

--- assignment1.cpp
+++ assignment1.cpp
@@ -2,7 +2,29 @@
 #include <cctype>
 #include <vector>
 
+// Initial Idea:
+// To find every possible combination by user input, It would be best to use recursion.
+// My hypothesis is that every time i run function 
 
+
+void combination (std::vector<std::string> & v, std::string r, std::string s = "") {
+  if (r.size() == 0) {
+    v.push_back(s);
+  }
+  for (int i=0; i< r.size(); i++) {
+    std::string temp_s = s;
+    std::string temp_r = r;
+    temp_r.erase(i,1);
+    temp_s = temp_s + r[i] + " ";
+    combination(v, temp_r, temp_s);
+  }
+}
+
+void printCombination(std::vector<std::string> & v) {
+  for (int i=0; i < v.size(); i++) {
+    std::cout << v[i] << std::endl;
+  }
+}
 
 int main () {
   std::string y = "";
@@ -12,19 +34,18 @@ int main () {
     std::cin >> x;
     for (int i=0; i< x.size(); i++) {
       if (!std::isalpha(x[i])) {
-        // std::cout << "reached";
         check = false;
+        break;
       } else {
         y += x[i];
       }
     }
   }
+  std::cout << "printing" << std::endl;
+  std::vector<std::string> v;
+  combination(v, y);
+  printCombination(v);
 
-  std::vector<std::string> v(y.size());
-  for (int i=0; i< v.size(); i++) {
-    v[i] = y[i];
-  }
 
-  
   return 0;
 }
