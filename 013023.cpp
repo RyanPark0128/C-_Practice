6bf4dbbc524c1a1928e4f12a433a9b9e66c5669e
RyanPark0128 <vazywon@hotmail.com>
2023-01-27 08:42:28 -0800

    013023 lecture

---
+++ 013023.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int fun(int A[], int n) {
+  if (n==1) {
+    return 
+  }
+}
+
+int main () {
+  return 0;
+}
+
+// n =5
+// 2+2+2+2+4 = 12
+
+// n = 6
+// 14
+
+// n = 7
+// 16
+
+// n*2 + 2
