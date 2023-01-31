2580290184620080a4ad6e437ca79dd9ab160ae2
RyanPark0128 <vazywon@hotmail.com>
2023-01-31 08:51:30 -0800

    directory clean up

--- 013023.cpp
+++ 013023.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <vector>
 
-int fun(int A[], int n) {
-  if (n==1) {
-    return 
+int part (std::vector<int> & v, int low, int high) {
+  int piv = v[low];
+
+  while (low < high) {
+    while (low < high && piv < v[high]) {
+      high--;
+    }
+    v[low] = v[high];
+    while (low < high && piv > v[low]) {
+      low++;
+    }
+    v[high] = v[low];
   }
+  v[low] = piv;
+  return low;
+}
+
+void qs (std::vector<int> & v, int low, int high) {
+  if (low >= high) {
+    return;
+  }
+  int pivot = part(v,low,high);
+  // qs(v,low,pivot-1);
+  // qs(v,pivot+1,high);
+}
+
+void qs(std::vector<int> & v) {
+  qs(v,0,v.size()-1);
 }
 
 int main () {
+  std::vector<int> v = {7,3,9,4,8,12,1};
+  qs(v);
+    for (auto x: v) {
+    std::cout << x << std::endl;
+  }
   return 0;
 }
 
