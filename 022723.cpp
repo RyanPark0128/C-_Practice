efa6cea10bb74e129866b45caafe6e18ccb9c15e
RyanPark0128 <vazywon@hotmail.com>
2023-02-22 23:04:28 -0800

    022723 lecture 3

--- 022723.cpp
+++ 022723.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 // memory management
 
+struct point {
+  int z,y;
+  void to_string() {
+
+  }
+};
+
+//example of swap function
+void swap (int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
 int main () {
 
 // * means memory address
@@ -16,6 +29,29 @@ int main () {
   // making reference to different type will cause error. pointer can only point to same type (You cant cast it).
 
 
-  std::cout << b;
+int c = 5;
+int d = 6;
+swap(&c, &d);
+
+// using "new" will create dynamic memory and responsible for managing memeory.
+// assign value
+int *x = new int(5);
+// frees up the memory.
+delete x;
+// set it to nullptr if you are done with it.
+a = nullptr;
+// assign random vaue;
+// int *x = new int;
+
+// memory leak if you dont have access to memory location.
+
+// to check if it is nullptr or not
+// if (!p)
+
+point *p = new point;
+p->y = 6;
+
+
+  std::cout << (*p).y << std::endl;
   return 0;
 }
