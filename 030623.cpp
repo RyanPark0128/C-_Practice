109e7d4d821e55c7943af51a7061ac0e47535a73
RyanPark0128 <vazywon@hotmail.com>
2023-03-03 10:16:28 -0800

    030623 lecture 2

---
+++ 030623.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "point.H"
+#include "my_vector.h"
+
+int main() {
+  // point p1(3,8);
+  // point p2 = p1;
+  // point p3 (13,2);
+  // p1 = p3;
+  // std::cout << p1 << std::endl;
+  my_vector(5);
+  return 0;
+}
